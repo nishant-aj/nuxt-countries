bb4a7dc8b6b0061042a445585e7348204e636a65
nishant-aj <nishant.jalan03@gmail.com>
2022-07-02 16:09:07 +0530

    Updated branch

--- app.vue
+++ app.vue
@@ -1,23 +1,13 @@
-<script setup lang="ts">
-  
-</script>
-
 <template>
   <div id="root">
     <Nav />
 
     <div class="container-fluid px-4">
       <div class="row">
-        <div v-for="i in [1, 2, 3, 4, 4, 1, 2, 4]" class="col-12 col-md-6 col-lg-3" :key="i">
+        <div v-for="i in [1, 2, 3, 4]" class="col-12 col-md-6 col-lg-3" :key="i">
           <CountryCard />
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<style>
-#root {
-  background: black;
-}
-</style>
